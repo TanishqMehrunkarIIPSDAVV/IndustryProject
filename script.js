1fdb7effc6662c09e988d9db4ca1fea7a63e8517
SimpPoseidon <mehrunkart@gmail.com>
2024-03-14 13:05:24 +0530

    start

---
+++ script.js
@@ -0,0 +1,41 @@
+const widthFunc=()=>
+{
+    const menu=document.querySelector("#menu");
+    const navbar=document.querySelector("#navbar-list");
+    let width=window.innerWidth;
+    if(width<800)
+    {
+        navbar.style.display="none";
+        menu.style.display="block";
+    }
+}
+
+
+const clicked=()=>
+{
+    const navbarMenu=document.querySelector("#navbar-list-menu");
+    if(navbarMenu.classList.contains("navbar-list-menu-clicked"))
+    {   
+        navbarMenu.classList.remove("navbar-list-menu-clicked");
+        navbarMenu.classList.add("navbar-list-menu");
+    }
+    else
+    {
+        navbarMenu.classList.remove("navbar-list-menu");
+        navbarMenu.classList.add("navbar-list-menu-clicked");
+    }
+}
+
+$(window).resize(function()
+{
+    const navbar=document.querySelector("#navbar-list");
+    const menu=document.querySelector("#menu");
+    const navbarMenu=document.querySelector("#navbar-list-menu");
+    menu.style.display="none";
+    navbar.style.display="flex";
+    widthFunc();
+});
+
+widthFunc();
+
+menu.addEventListener("click",clicked);
