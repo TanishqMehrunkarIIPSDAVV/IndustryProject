ff17c158df62ece6d1fc123982fd005f09708c35
SimpPoseidon <mehrunkart@gmail.com>
2024-05-31 13:12:15 +0530

    zakie is required here

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-document.querySelector("#logo").addEventListener("click", () => {
-  window.location.reload();
-});
+let index=1;
+const leftButton=()=>
+{
+    if(index<=1)
+    {
+        index=9;
+    }
+    else
+    {
+        index--;
+    }
+    document.querySelector(".carousel-image").setAttribute("src",`./images/image${index}.jpg`);
+}
+
+const rightButton=()=>
+{
+    if(index>=9)
+    {
+        index=1;
+    }
+    else
+    {
+        index++;
+    }
+    document.querySelector(".carousel-image").setAttribute("src",`./images/image${index}.jpg`);
+}
+
+document.querySelector(".left").addEventListener("click",leftButton);
+document.querySelector(".right").addEventListener("click",rightButton);
