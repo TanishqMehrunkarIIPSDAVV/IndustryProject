b0cfba87aa8ab3a5c1505174ebb292a5ab274491
Kartikey2011yadav <nykay10968@gmail.com>
2024-06-08 13:10:34 +0530

    navbar dug solved

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,6 @@
 (function ($) {
 
 	
-	$('.filters ul li').click(function(){
-        $('.filters ul li').removeClass('active');
-        $(this).addClass('active');
-          
-          var data = $(this).attr('data-filter');
-          $grid.isotope({
-            filter: data
-          })
-        });
-
-        var $grid = $(".grid").isotope({
-          itemSelector: ".all",
-          percentPosition: true,
-          masonry: {
-            columnWidth: ".all"
-          }
-        })
 
 
 	const Accordion = {
